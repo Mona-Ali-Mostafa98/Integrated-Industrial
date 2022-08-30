3993f1c732f3ddc9d3be4ccb204b2a78d81b3ff5
Mona Ali Mostafa <monaali980628@gmail.com>
2022-08-30 14:07:53 +0200

    edit in validation of category

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -30,7 +30,7 @@ class StoreCategoryRequest extends FormRequest
             'category_name' =>['required','string','max:255' , Rule::unique('categories')->ignore($this->category)],
             'category_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'parent_id' => ['nullable','exists:categories,id'],
-            'category_order'=>['nullable' ,  Rule::in(['main_category', 'sub_category' ,'third_category'])],
+            'category_order'=>['nullable' ,  Rule::in(['قسم رئيسى', 'قسم فرعى' ,'قسم فرعى من قسم فرعى أخر'])],
             'status'=>['required' ,  Rule::in(['عرض', 'أخفاء'])]
         ];
     }
